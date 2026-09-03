e72a4e912ad24ea9ddf2498dcc0d395b963ea7a6
mailztoganesh-civil <mailztoganesh@gmail.com>
2026-09-04 03:51:32 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ const ASSETS = [
   './beams.js',
   './app.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-512-maskable.png'
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png'
 ];
 
 self.addEventListener('install', event => {
